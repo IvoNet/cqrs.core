ea00f4cb149fdf6c4faa15e53f46821464c5fa82
Ivo Woltring <ivo@ivonet.nl>
2022-05-30 18:45:25 +0200

    added event store implementation added custom exceptions

---
+++ src/main/java/nl/ivonet/cqrs/core/exceptions/AggregateNotFoundException.java
@@ -0,0 +1,7 @@
+package nl.ivonet.cqrs.core.exceptions;
+
+public class AggregateNotFoundException extends RuntimeException {
+    public AggregateNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/nl/ivonet/cqrs/core/exceptions/ConcurrencyException.java
@@ -0,0 +1,4 @@
+package nl.ivonet.cqrs.core.exceptions;
+
+public class ConcurrencyException extends RuntimeException {
+}
