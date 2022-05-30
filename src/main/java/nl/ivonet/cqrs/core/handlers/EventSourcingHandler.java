a858e2ceddeb9af9b4b7908d4ef72ac3a95eaac7
Ivo Woltring <ivo@ivonet.nl>
2022-05-30 18:49:49 +0200

    added EventSourcingHandler interface

---
+++ src/main/java/nl/ivonet/cqrs/core/handlers/EventSourcingHandler.java
@@ -0,0 +1,8 @@
+package nl.ivonet.cqrs.core.handlers;
+
+import nl.ivonet.cqrs.core.domain.AggregateRoot;
+
+public interface EventSourcingHandler<T> {
+    void save(AggregateRoot aggregateRoot);
+    T getById(String id);
+}
