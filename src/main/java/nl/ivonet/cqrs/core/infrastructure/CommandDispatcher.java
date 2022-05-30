a60dd87df551d5914b2dd3074291238a54f0760b
Ivo Woltring <ivo@ivonet.nl>
2022-05-30 20:47:18 +0200

    implemented the AccountingCommandHandler

--- src/main/java/nl/ivonet/cqrs/core/infrastructure/CommandDispatcher.java
+++ src/main/java/nl/ivonet/cqrs/core/infrastructure/CommandDispatcher.java
@@ -4,7 +4,7 @@ import nl.ivonet.cqrs.core.commands.BaseCommand;
 import nl.ivonet.cqrs.core.commands.CommandHandlerMethod;
 
 /**
- * Mediator.
+ * (Abstract) Mediator.
  */
 public interface CommandDispatcher {
     <T extends BaseCommand> void registerHandler(Class<T> type, CommandHandlerMethod<T> handler);
